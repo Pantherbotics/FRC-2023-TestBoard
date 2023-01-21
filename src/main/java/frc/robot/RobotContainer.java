7cd90b0a05ab3801983ead37227cf3d48b7aacd2
Scott Wegley <scbrwegley@protonmail.ch>
2023-01-21 14:24:23 -0800

    added robotcontainer w joystick + buttons

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,25 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+public class RobotContainer {
+
+    private final Joystick pJoy = new Joystick(Constants.OIConstants.pJoyID);
+    private final JoystickButton joyBA = new JoystickButton(pJoy, 1); //Button A
+    private final JoystickButton joyBB = new JoystickButton(pJoy, 2); //Button B
+    private final JoystickButton joyBX = new JoystickButton(pJoy, 3); //Button X
+    private final JoystickButton joyBY = new JoystickButton(pJoy, 4); //Button Y
+
+    public RobotContainer(Robot robot){
+        configureButtonBindings();
+    }
+
+    public void configureButtonBindings(){
+        
+    }
+
+    public void updateSmartDashboard(){
+        
+    }
+}
