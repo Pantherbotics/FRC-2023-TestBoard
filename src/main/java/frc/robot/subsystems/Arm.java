97a8bef7d0962684186527648d5eb0e9f849e56a
Scott Wegley <scbrwegley@protonmail.ch>
2023-01-22 12:36:42 -0800

    added servos

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -9,7 +9,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Arm extends SubsystemBase {
+  private final PWMSparkMax rotateA, rotateB, wristA, wristB;
+
   public Arm() {
+    rotateA = new PWMSparkMax(Constants.ArmConstants.rotationA);
+    rotateB = new PWMSparkMax(Constants.ArmConstants.rotationB);
+    wristA = new PWMSparkMax(Constants.ArmConstants.wristA);
+    wristB = new PWMSparkMax(Constants.ArmConstants.wristB);
+  }
   @Override
   public void periodic() {
   }
