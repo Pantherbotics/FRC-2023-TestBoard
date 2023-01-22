341cfe567661619761899b05c42f770f781aadfa
Scott Wegley <scbrwegley@protonmail.ch>
2023-01-22 12:37:55 -0800

    more constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,5 +3,15 @@ package frc.robot;
 public class Constants {
     public static final class OIConstants{
         public static final int pJoyID = 0;
+        public static final int kDriverXL = 0;
+        public static final int kDriverYL = 1;
+    }
+
+    public static final class ArmConstants {
+        public static final int rotationA = 0;
+        public static final int rotationB = 1;
+        public static final int wristA = 2;
+        public static final int wristB = 3;
+
     }
 }
