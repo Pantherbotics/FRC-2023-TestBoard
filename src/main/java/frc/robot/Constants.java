6b07b9c892fc0a0f3fccfcfa895758c3cb55ba1d
Scott Wegley <scbrwegley@protonmail.ch>
2023-01-23 13:25:06 -0800

    hardcoded 6 channels

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,7 +8,13 @@ public class Constants {
     }
 
     public static final class ArmConstants {
-        public static final int rotationChannel = 0;
-        public static final int flexChannel = 1;
+        public static final int rotationChannelA = 0;
+        public static final int rotationChannelB = 1;
+        public static final int rotationChannelC = 2;
+        public static final int rotationChannelD = 3;
+        public static final int rotationChannelE = 4;
+        public static final int rotationChannelF = 5;
+        public static final int flexChannelA = 6;
+        public static final int flexChannelB = 7;
     }
 }
--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -9,11 +9,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Arm extends SubsystemBase {
-  private final PWMSparkMax rotateChannel, flexChannel;
+  private final PWMSparkMax rotateChannelA, rotateChannelB, rotateChannelC, rotateChannelD, rotateChannelE, rotateChannelF;
+  private final PWMSparkMax flexChannelA, flexChannelB;
 
   public Arm() {
-    rotateChannel = new PWMSparkMax(Constants.ArmConstants.rotationChannel);
-    flexChannel = new PWMSparkMax(Constants.ArmConstants.flexChannel);
+    rotateChannelA = new PWMSparkMax(Constants.ArmConstants.rotationChannelA);
+    rotateChannelB = new PWMSparkMax(Constants.ArmConstants.rotationChannelB);
+    rotateChannelC = new PWMSparkMax(Constants.ArmConstants.rotationChannelC);
+    rotateChannelD = new PWMSparkMax(Constants.ArmConstants.rotationChannelD);
+    rotateChannelE = new PWMSparkMax(Constants.ArmConstants.rotationChannelE);
+    rotateChannelF = new PWMSparkMax(Constants.ArmConstants.rotationChannelF);
+    flexChannelA = new PWMSparkMax(Constants.ArmConstants.flexChannelA);
+    flexChannelB = new PWMSparkMax(Constants.ArmConstants.flexChannelB);
   }
 
   public void rotate(double speed) {
@@ -21,7 +28,12 @@ public class Arm extends SubsystemBase {
     if (speed != 0 && Math.abs(speed) > 1) {
       speed /= Math.abs(speed);
     }
-    rotateChannel.set(speed);
+    rotateChannelA.set(speed);
+    rotateChannelB.set(speed);
+    rotateChannelC.set(speed);
+    rotateChannelD.set(speed);
+    rotateChannelE.set(speed);
+    rotateChannelF.set(speed);
   }
 
   public void flex(double speed) {
@@ -29,7 +41,8 @@ public class Arm extends SubsystemBase {
     if (speed != 0 && Math.abs(speed) > 1) {
       speed /= Math.abs(speed);
     }
-    flexChannel.set(speed);
+    flexChannelA.set(speed);
+    flexChannelB.set(speed);
   }
 
   public void stop() {
