5c572957a8f7386b7ccfd1d71e3c76dc28229255
Scott Wegley <scbrwegley@protonmail.ch>
2023-01-23 12:57:22 -0800

    turn main branch into a template, arm getting it's own branch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,17 +1,12 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-import frc.robot.commands.RunArm;
-import frc.robot.subsystems.Arm;
 
 public class RobotContainer {
 
-    private final Arm arm = new Arm();
-    
     private final Joystick pJoy = new Joystick(Constants.OIConstants.pJoyID);
 
     public RobotContainer(Robot robot){
-        arm.setDefaultCommand(new RunArm(arm, pJoy));
         configureButtonBindings();
     }
 
