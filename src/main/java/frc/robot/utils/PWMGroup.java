c3e98521ee00426538dd500783d87786b2121b0c
Scott Wegley <scbrwegley@protonmail.ch>
2023-01-23 13:52:09 -0800

    started supporting functions

--- src/main/java/frc/robot/utils/PWMGroup.java
+++ src/main/java/frc/robot/utils/PWMGroup.java
@@ -4,9 +4,15 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 public class PWMGroup {
     private PWMSparkMax[] members = new PWMSparkMax[] {};
+    private boolean[] exists = new boolean[] {};
+    private int totalMembers = 0;
     
     public PWMGroup(int memberCount){
         members = new PWMSparkMax[memberCount];
+        exists = new boolean[memberCount];
+        for (int i = 0; i < exists.length; i++) {
+            exists[i] = false;
+        }
     }
 
     public PWMGroup(int memberCount, int startingChannel){
@@ -21,5 +27,10 @@ public class PWMGroup {
             throw new IndexOutOfBoundsException("Invalid Spark Index! The max index you can access for this PWMGroup is" + members.length);
         }
         members[sparkIndex] = new PWMSparkMax(channel);
+        exists[sparkIndex] = true;
+    }
+
+    public void add(PWMSparkMax toAdd){
+
     }
 }
